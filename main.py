40448da20185e9515d2f3dddeb187f0a911bb80c
Hayyan67 <humairabaig81@gmail.com>
2024-09-08 15:15:56 +0500

    initial commit

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ turtle.Screen().bgcolor('Orange')
 t= turtle.Turtle()
 t.shape('turtle')
 t.color('green')
-
-for c in range(3):
-    t.forward(200)
-    t.left(120)
+t.penup()
+size=0
+t.goto(-200,140)
+t.pendown()
+for c in range(2):
+    t.forward(500)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
 
 t.penup()
-t.left(90)
-t.forward(100)
-t.right(90)
+t.goto(-180,90)
 t.pendown()
-for c in range(3):
-    t.forward(200)
-    t.right(120)
-
+for c in range(2):
+    t.forward(420)
+    t.right(90)
+    t.forward(420)
+    t.right(90)
 turtle.done()
