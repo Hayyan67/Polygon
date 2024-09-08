68ef760dcc1298ab578d9ad9975d57a5b08e911d
Hayyan67 <humairabaig81@gmail.com>
2024-09-08 14:47:44 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor('Orange')
+t= turtle.Turtle()
+t.shape('turtle')
+t.color('green')
+
+for c in range(4):
+    t.forward(200)
+    t.right(90)
+
+turtle.done()
